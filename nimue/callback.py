8fa78f1d0d753954b35f2929d23212416235663d
James Lucas <73145226+jlucasdba@users.noreply.github.com>
2021-10-29 19:11:32 -0500

    Make healthcheck_callback_base public
    The other two built-in callbacks use this function with different
    queries. By making it public, a user can retain the default
    functionality and define only a new query.

--- nimue/callback.py
+++ nimue/callback.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2021 James Lucas
 
 def healthcheck_callback_std(conn,dbmodule,logger):
-  return _healthcheck_callback_base(conn,dbmodule,logger,"SELECT 1")
+  return healthcheck_callback_base(conn,dbmodule,logger,"SELECT 1")
 
 def healthcheck_callback_oracle(conn):
-  return _healthcheck_callback_base(conn,dbmodule,logger,"SELECT 1 FROM DUAL")
+  return healthcheck_callback_base(conn,dbmodule,logger,"SELECT 1 FROM DUAL")
 
-def _healthcheck_callback_base(conn,dbmodule,logger,query):
+def healthcheck_callback_base(conn,dbmodule,logger,query):
   try:
     with conn.cursor() as curs:
       curs.execute(query)
