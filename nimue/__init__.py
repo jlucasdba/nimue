095ee0973e46e46215aecfd59cf88097ea94af1b
James Lucas <73145226+jlucasdba@users.noreply.github.com>
2021-11-08 18:33:02 -0600

    Further import __init__ docstring with more detail

--- nimue/__init__.py
+++ nimue/__init__.py
@@ -1,13 +1,26 @@
 # Copyright (c) 2021 James Lucas
 
 """
-Database connection pooler for Python. Import the main package with::
+Database connection pooler for Python.
+
+For basic usage, import the main package with::
 
   import nimue
 
 Optionally, import submodules with additional functionality if needed::
 
   import nimue.callback
+
+Public Classes For Direct Use
+=============================
+* ``NimueConnnectionPool`` The main connection pool class. Initializes a database connection pool.
+
+Other Public Classes
+====================
+* ``NimueConnection`` Returned by getconnection(). Wraps a raw database connection, and returns it to the pool when closed.
+* ``NimueConnectionPoolStats`` Returned by the pool getstats() method. Contains fields describing pool runtime statistics, suitable for monitoring.
+
+See nimue.nimue for the details API.
 """
 
 from nimue.nimue import *
