c09997829e3b24fb57f081d046a3ce193b7dad90
James Lucas <73145226+jlucasdba@users.noreply.github.com>
2022-01-19 16:42:53 -0600

    Remove redundant pass statements - docstrings are enough

--- nimue/error.py
+++ nimue/error.py
@@ -4,16 +4,12 @@ Exception classes for Nimue.
 
 class NimueInvalidParameterValue(Exception):
   """Indicates that a value passed for a parameter was not accepted."""
-  pass
 
 class NimueDBModuleFailure(Exception):
   """Indicates failure to identify the underlying dbmodule in use."""
-  pass
 
 class NimuePoolClosedError(Exception):
   """Indicates an operation was attempted on a pool that was already closed."""
-  pass
 
 class NimueNoConnectionAvailable(Exception):
   """Raised when getconnection() fails to return a connection, due to timeout or another reason."""
-  pass
